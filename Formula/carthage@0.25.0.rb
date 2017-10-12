60f215b81fb2a17dd4dbfc485f837239fec1ca6a
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-10-12 14:30:46 +1100

    Add `carthage@0.25.0.rb`

---
+++ Formula/carthage@0.25.0.rb
@@ -0,0 +1,29 @@
+class CarthageAt0_25_0 < Formula
+  desc "Decentralized dependency manager for Cocoa"
+  homepage "https://github.com/Carthage/Carthage"
+  url "https://github.com/Carthage/Carthage.git",
+      :tag => "0.25.0",
+      :revision => "32a764f9bfe4765f32e11cf7fc580e7f8795d560",
+      :shallow => false
+  head "https://github.com/Carthage/Carthage.git", :shallow => false
+
+  bottle do
+    cellar :any
+    sha256 "a079f446bf37b176f6706a1c62a6cba8494ca0cc3c5368f21b5eafd800f48646" => :high_sierra
+    sha256 "f3ff7f9c4cf0394633141cb69d3dea35dd9af81bb0325fb67548bfbe1550d8e7" => :sierra
+  end
+
+  depends_on :xcode => ["8.3", :build]
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}"
+    bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+    zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+    fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
+  end
+
+  test do
+    (testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+    system bin/"carthage", "update"
+  end
+end
